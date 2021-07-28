2eef723e8ab019e95798fd12738bfdd9a5021879
gaurav <aadeezcity@gmail.com>
2021-07-28 17:29:56 +0530

    Form values submission

--- src/components/FormikUse/YoutubeForm.js
+++ src/components/FormikUse/YoutubeForm.js
@@ -1,35 +1,57 @@
-import React from 'react'
-import styles from './Formik.module.css'
-import { useFormik } from 'formik'
+import React from "react";
+import styles from "./Formik.module.css";
+import { useFormik } from "formik";
 const YoutubeForm = () => {
-    
-    const formik = useFormik({
-        initialValues: {
-            name:'',
-            email:'',
-            channel:''
-        }
-    })
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: "",
+    },
+    onSubmit:values => {
+        console.log(values,"form data");
+    }
+  });
 
-    console.log('form values', formik.values);
+//   console.log("form values", formik.values);
 
-    return (
-        <div className={styles.container}>
-            <form>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" name='name' className={styles.input} onChange={formik.handleChange} value={formik.values.name} />
+  return (
+    <div className={styles.container}>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className={styles.input}
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
 
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name='email'  className={styles.input} onChange={formik.handleChange} value={formik.values.email}  />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          className={styles.input}
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
 
-                <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" name='channel'  className={styles.input} onChange={formik.handleChange} value={formik.values.channel} />
+        <label htmlFor="channel">Channel</label>
+        <input
+          type="text"
+          id="channel"
+          name="channel"
+          className={styles.input}
+          onChange={formik.handleChange}
+          value={formik.values.channel}
+        />
 
-                <button className={styles.submit}>Submit</button>
-                
-            </form>
-        </div>
-    )
-}
+        <button type="submit "className={styles.submit} >Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default YoutubeForm
+export default YoutubeForm;
